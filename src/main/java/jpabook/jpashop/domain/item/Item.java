ba14330cd12b67f79ea057f66cfe1d8f72194fe2
LeeGun159 <tdyfuh7536@naver.com>
2025-01-03 18:32:53 +0900

    -상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter 
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
@@ -25,4 +26,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로작 =//
+
+    /**
+     * stock 증가
+     */
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    /**
+     * stock 감소
+     */
+    public void removeStock (int quantity) {
+        int restock = this.stockQuantity - quantity;
+        if (restock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restock;
+    }
 }
