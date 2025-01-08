a4ed9ee360a2273693f985be894a9a69512e821b
LeeGun159 <tdyfuh7536@naver.com>
2025-01-08 17:21:52 +0900

    -Hibernate5 추가

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,10 @@
 package jpabook.jpashop;
 
+
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -11,4 +14,17 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class ,  args);
 	}
 
+	@Bean
+	Hibernate5JakartaModule hibernate5JakartaModule() {
+		//강제 LAZY 로딩 하는 구문 but 이렇게 하면 엔티티를 외부로 노출하게 돼서 쓰지말것
+//		Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+//		hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return new Hibernate5JakartaModule();
+	}
+
+	//스프링 부트 3.0 이하
+//	@Bean
+//	Hibernate5Module hibernate5Module() {
+//		return new Hibernate5Module();
+//	}
 }
